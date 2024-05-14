83ee2c3a6548ffa7a3ba5d43e919ef8fce2785cc
Moon-12 <ashwijanayak@gmail.com>
2024-05-14 15:44:12 -0400

    navbar produced with json data and styling added

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,28 @@
 import "./NavBar.css";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { ui_data } from "../../env/common_ui_metadata";
+import { fetchImagesByTag } from "../../redux/slice/imageSlice";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
 const NavBar = () => {
+  const { navLinks } = ui_data;
+  const { type } = useParams();
+  console.log(type);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchImagesByTag(type));
+  }, [type]);
+
   return (
     <nav className="nav-container">
-      <Link to={"images/mountain"}>Mountain</Link>
-      <Link to={"images/beach"}>Beaches</Link>
-      <Link to={"images/bird"}>Birds</Link>
-      <Link to={"images/food"}>Food</Link>
+      {navLinks.map((ele) => {
+        return (
+          <Link key={ele.url} to={ele.url}>
+            {ele.content}
+          </Link>
+        );
+      })}
     </nav>
   );
 };
