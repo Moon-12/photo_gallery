017e7d0ef3bca64fb7986d3606942b09ead7f410
Moon-12 <ashwijanayak@gmail.com>
2024-05-14 15:44:41 -0400

    pictures displayed dynamically

--- src/components/Pictures/Pictures.jsx
+++ src/components/Pictures/Pictures.jsx
@@ -1,16 +1,22 @@
+import { useParams } from "react-router-dom";
 import "./Pictures.css";
+import { useSelector } from "react-redux";
 const Picture = () => {
+  const { type } = useParams();
+  const images = useSelector((state) => state.imageReducer.images);
+
   return (
     <div>
-      <h4> pictures</h4>
+      <h4> {type} pictures</h4>
       <div className="img-container">
-        <img src="https://farm66.staticflickr.com/65535/53712117615_a0ffc53ee4_m.jpg"></img>
-        <img src="https://farm66.staticflickr.com/65535/53712117615_a0ffc53ee4_m.jpg"></img>
-        <img src="https://farm66.staticflickr.com/65535/53712117615_a0ffc53ee4_m.jpg"></img>
-        <img src="https://farm66.staticflickr.com/65535/53712117615_a0ffc53ee4_m.jpg"></img>
-        <img src="https://farm66.staticflickr.com/65535/53712117615_a0ffc53ee4_m.jpg"></img>
-        <img src="https://farm66.staticflickr.com/65535/53712117615_a0ffc53ee4_m.jpg"></img>
-        <img src="https://farm66.staticflickr.com/65535/53712117615_a0ffc53ee4_m.jpg"></img>
+        {images.map((img) => {
+          const { server, id, secret } = img;
+          return (
+            <img
+              src={`https://farm66.staticflickr.com/${server}/${id}_${secret}_m.jpg`}
+            />
+          );
+        })}
       </div>
     </div>
   );
