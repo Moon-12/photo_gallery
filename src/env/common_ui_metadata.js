95dd727edfd1e2d3bc8a1f341781023c0f9bce6d
Moon-12 <ashwijanayak@gmail.com>
2024-05-14 15:43:54 -0400

    json data added

---
+++ src/env/common_ui_metadata.js
@@ -0,0 +1,8 @@
+export const ui_data = {
+  navLinks: [
+    { url: "images/mountain", content: "Mountain", tag: "mountain" },
+    { url: "images/beach", content: "Beaches", tag: "beach" },
+    { url: "images/bird", content: "Birds", tag: "bird" },
+    { url: "images/food", content: "Food", tag: "food" },
+  ],
+};
